e29525531eb0e6e26ec361854b4cdf8f056038cd
AkMedIm <mohamedakdim1@gmail.com>
2017-11-06 15:36:23 +0000

    full width slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 function openNav() {
-    document.getElementById("myNav").style.height = "100%";
+  document.getElementById("open").style.transition="0";
+  document.getElementById("open").classList.add("hidden");
+  document.getElementById('close').classList.remove("hidden");
+  document.getElementById("myNav").style.height = "100%";
+
+
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
+  document.getElementById("close").style.transition="0";
+  document.getElementById('close').classList.add("hidden");
+  document.getElementById("open").classList.remove("hidden");
+  document.getElementById("myNav").style.height = "0%";
+
 }
